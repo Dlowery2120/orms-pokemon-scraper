cd6b113b08aab6ceacd7cf973ca9aaef8efe0ff8
Devante Lowery <dlowery2120@gmail.com>
2021-03-04 16:30:17 -0600

    commit tree 2e2270cda32760222da152f0931303d4dbbdd862

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,33 @@
+# class Pokemon
+#     attr_accessor :id, :name, :type, :db
+
+#     def initialize (id:, name:, type:, db:)
+#         @id = id
+#         @name = name
+#         @type = type
+#         @db = db
+#     end
+
+#     def self.save(name, type, db)
+#         db.execute("INSERT INTO pokemon (name, type, db) VALUES (pikachu, electric, db)")
+#     end
+    
+# end
+# # insert into person (id, name) values (?, ?)
+
 class Pokemon
-end
+    attr_accessor :id, :name, :type, :db
+      def initialize(id:, name:, type:, db:)
+          @id = id
+          @name = name
+          @type = type
+          @db = db
+      end
+      def self.save(name, type, db)
+      db.execute("INSERT INTO pokemon(name,type) VALUES(?, ?)", name, type) 
+      end
+      def self.find(id, db)
+      pokemon = db.execute("SELECT * FROM pokemon WHERE id= ?", id)
+      new_pokemon = Pokemon.new(id:pokemon[0][0] , name:pokemon[0][1], type:pokemon[0][2], db: db)
+      end
+  end
